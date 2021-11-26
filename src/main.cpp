00ad602c787c5fbf79e6e35b101391cc3fa3e7df
JaeMin Shin <alex9395@naver.com>
2021-11-27 01:34:11 +0900

    main.cpp test code remove

--- src/main.cpp
+++ src/main.cpp
@@ -5,7 +5,6 @@
 #include <cassert>
 #include <cstdio>
 #include <tester/Forwarder.hpp>
-#include <tester/Parser.hpp>
 
 const std::string IP_ADDR = "127.8.8.8";
 const uint16_t PORT       = 4997;
@@ -18,12 +17,9 @@ const unsigned int BUFSIZE = 16777216;
 
 int main(int argc, char ** argv)
 {
-  Parser p;
-  p.set_inj_act("adv_BU_rrc_release_suspend");
-  /*
     Forwarder forwarder(GNB_ADDR, GNB_PORT, IP_ADDR, PORT);
 
     forwarder.do_work();
-    */
+
     return 0;
 }
