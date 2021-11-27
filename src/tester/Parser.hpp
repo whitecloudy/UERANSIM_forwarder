73a9cf58860aa93f3d77b7eae76e247c98b59830
Doubb <doubele112@naver.com>
2021-11-27 15:41:37 +0900

    resolve conflicts

--- src/tester/Parser.hpp
+++ src/tester/Parser.hpp
@@ -9,11 +9,7 @@
 #include <asn/rrc/ASN_RRC_BCCH-DL-SCH-Message.h>
 #include <asn/rrc/ASN_RRC_DL-CCCH-Message.h>
 #include <asn/rrc/ASN_RRC_DL-DCCH-Message.h>
-<<<<<<< HEAD
 #include <asn/rrc/ASN_RRC_PCCH-Message.h>
-=======
-#include <asn/rrc/ASN_RRC_PCCH-Message.h> 
->>>>>>> 0db43be4836c1db268e5acb6244fef463d653e18
 
 #define TOKEN_NUM 3
 
@@ -31,75 +27,6 @@ static set<string> pcch_set = {"paging_tmsi"};
 
 
 static map<string, uint32_t> msg_type_map = {
-<<<<<<< HEAD
-        {"rrc_sm_complete", ASN_RRC_UL_DCCH_MessageType__c1_PR_securityModeComplete},
-        {"rrc_release_suspend", ASN_RRC_DL_DCCH_MessageType__c1_PR_rrcRelease},
-        {"rrc_release", ASN_RRC_DL_DCCH_MessageType__c1_PR_rrcRelease},
-        {"rrc_setup", ASN_RRC_DL_CCCH_MessageType__c1_PR_rrcSetup},
-        {"rrc_sm_command", ASN_RRC_DL_DCCH_MessageType__c1_PR_securityModeCommand},
-        {"rrc_setup_complete", ASN_RRC_UL_DCCH_MessageType__c1_PR_rrcSetupComplete},
-        {"rrc_resume_req", ASN_RRC_UL_CCCH_MessageType__c1_PR_rrcResumeRequest},
-        {"pagig_tmsi", ASN_RRC_PCCH_MessageType__c1_PR_paging}
-};
-
-class Parser {
-
-    class Action {
-
-    public:
-        Action(string act_name);
-        ~Action() {
-            msg_data.clear();
-        }
-
-    public:
-        ChannelType ch_type;
-        DirectionType dir_type;
-        uint32_t msg_type_id;
-        ReleaseType release_type;
-        string dst;
-        map<string, uint64_t> msg_data;
-    };
-
-public:
-    ~Parser() {
-        delete inj_act;
-    }
-
-    void set_inj_act(string act_name);
-    void set_inj_msg_data();
-
-    ChannelType get_ch_type() {
-        return inj_act->ch_type;
-    }
-    DirectionType get_dir_type(){
-        return inj_act->dir_type;
-    }
-    uint32_t get_msg_type_id()
-    {
-        return inj_act->msg_type_id;
-    }
-    ReleaseType get_release_type()
-    {
-        return inj_act->release_type;
-    }
-    string get_dst()
-    {
-        return inj_act->dst;
-    }
-    map<string, uint64_t>* get_msg_data_map()
-    {
-        return &inj_act->msg_data;
-    }
-
-    void record_recv_msg_data(string data_name, uint64_t data_value);
-    string set_recv_act_string(uint32_t id, ChannelType c, DirectionType d, string s);
-
-private:
-    Action* inj_act;
-    map<string, uint64_t> data_recorder;
-};
-=======
   {"rrc_sm_complete", ASN_RRC_UL_DCCH_MessageType__c1_PR_securityModeComplete},
   {"rrc_release_suspend", ASN_RRC_DL_DCCH_MessageType__c1_PR_rrcRelease}, 
   {"rrc_release", ASN_RRC_DL_DCCH_MessageType__c1_PR_rrcRelease}, 
@@ -167,4 +94,3 @@ private:
   Action* inj_act;
   map<string, uint64_t> data_recorder;
 };
->>>>>>> 0db43be4836c1db268e5acb6244fef463d653e18
