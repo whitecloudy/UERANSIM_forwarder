8f3203eb2b10cc134f340894cfa2c07ba0e7170a
doubele112 <doubele112@naver.com>
2021-11-28 13:53:56 +0900

    update state manager (#7)

--- src/tester/StateManager.hpp
+++ src/tester/StateManager.hpp
@@ -16,15 +16,32 @@ typedef enum State_type
     Success_e
 } State_type_t;
 
+typedef enum Chan_type
+{
+    True_e,
+    False_e
+} Chan_type_t;
+
 class StateManager {
 
 public:
     void set_testcase(string filepath); // Read and analyze TestCase
     void receive_string(string input); // Receive string and discern it
     void change_state(); // If conditions are fulfilled, change state (Action <-> Listen)
+
+    Chan_type_t get_chanUB_state() {
+        return chanUB;
+    };
+
+    Chan_type_t get_chanBU_state() {
+        return chanBU;
+    };
+
     string send_act_string(); // Send action string
 
     State_type_t state = Listen_e;
+    Chan_type_t chanUB = False_e;
+    Chan_type_t chanBU = False_e;
 };
 
 #endif
